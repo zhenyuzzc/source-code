a2a996c1c84a189fc50ba24c179338959b9cb7f0
zhenyuzzc <zhenchao_zhu@qq.com>
2021-09-19 23:39:03 +0800

    第二次编写

--- 6/6.11.py
+++ 6/6.11.py
@@ -15,7 +15,18 @@ cities = {
         'fact':'湿热',
     }
 }
+cities['hainan']={
+        'country':'china',
+        'populatino':'200000',
+        'fact':'水果',}
 for name,value in cities.items():
-    print(name.title())
+    print(f"所在城市{name.title()}")
     for k,v in value.items():
         print("\t",k,v)
+print()
+for name,value in cities.items():
+    print(f"所在城市{name.title()}")
+    print(f"\tcountry：{value['country']} / populatino：{value['populatino']} / fact：{value['fact']}\n")
+
+value = cities.get('tainjing','不存在')
+print(value)
