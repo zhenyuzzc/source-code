59a7009cf3b74d4f687db7ba1903b6c36fa6699b
zhenyuzzc <zehnchao_zhu@qq.com>
2021-09-20 17:24:11 +0800

    第三次编码

---
+++ Python/样例/列表和字典创建副本并小写.py
@@ -0,0 +1,19 @@
+lists = ['tom','Jack','luCy','lily','jErry','anna']
+
+dics = {'jack':'python','Lucy':'jaVa','jeRry':'rUby','lily':'c#',}
+
+new_lists = []
+
+for i in lists:
+
+    new_lists.append(i.lower())
+
+print(new_lists)
+
+new_dics = {}
+
+for i,j in dics.items():
+
+    new_dics[i.lower()] = j.lower()
+
+print(new_dics)
