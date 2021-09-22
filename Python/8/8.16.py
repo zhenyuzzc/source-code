59d29564ec76513559562aebcac54b545d91fa2d
zhenyuzzc <zehnchao_zhu@qq.com>
2021-09-22 21:00:40 +0800

    第一次编码

---
+++ Python/8/8.16.py
@@ -0,0 +1,7 @@
+from hs import *
+
+car = cars('斯巴鲁','傲虎',
+            颜色='珍珠白',
+            驾驶辅助系统='视驭')
+
+show_messages(car)
---
+++ Python/8/hs.py
@@ -0,0 +1,8 @@
+def cars(pb,cx,**cs):
+    cs['品牌'] = pb
+    cs['车型'] = cx
+    return cs
+
+def show_messages(txt):
+    print(txt)
+
